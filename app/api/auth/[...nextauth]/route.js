d1d520b6afa0605f245d8efbf67ec8b699172e57
LeafusBonesboro <108142251+LeafusBonesboro@users.noreply.github.com>
2025-09-30 03:43:28 -0400

    fixed google log in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
-
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
